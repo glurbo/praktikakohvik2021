187e734ecccd75c98b709ac58e837834fefbb075
Germo Linder <germo.linder@gmail.com>
2021-02-01 16:10:37 +0200

    form stuff

--- src/pages/form.js
+++ src/pages/form.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import Layout from "../components/common/layout/layout"
 import Form from "../components/sections/form"
 
-import React from "react"
 
 export default class FormPage extends React.Component {
   state = {
