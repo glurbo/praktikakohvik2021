b4bfae8f6ed3dad548cc171963e674185cc757b1
Germo Linder <germo.linder@gmail.com>
2021-02-01 16:09:14 +0200

    form stuff

--- src/components/sections/form.js
+++ src/components/sections/form.js
@@ -1,8 +1,54 @@
 import React from "react"
 import styled from "styled-components"
 
-const Form = () => {
-
-}
-
-export default Form
+export default class Form extends React.Component {
+    state = {
+      firstName: "",
+      lastName: "",
+      emailAddress: "",
+    }
+  
+    handleInputChange = event => {
+      const target = event.target
+      const value = target.value
+      const name = target.name
+      this.setState({
+        [name]: value,
+      })
+    }
+  
+    render() {
+      return (
+        <form>
+          <label>
+            Eesnimi
+            <input 
+              type="text" 
+              name="firstName" 
+              value={this.state.firstName}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Perenimi
+            <input 
+              type="text" 
+              name="lastName" 
+              value={this.state.lastName}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Email
+            <input 
+              type="text" 
+              name="emailAddress" 
+              value={this.state.emailAddress}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      )
+    }
+  }
