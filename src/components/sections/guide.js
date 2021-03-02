672361144b605d96e0a008ffbfc5eb76c1a397be
Germo Linder <germo.linder@gmail.com>
2021-03-02 21:11:35 +0200

    guide, companies

--- src/components/sections/guide.js
+++ src/components/sections/guide.js
@@ -15,6 +15,18 @@ const Guide = () => {
                 </Flex>
                 <Flex>
                     <TextGroup>
+                    <Title>Keegi liiga vali või vaikne?</Title>
+                        <p>Discordis on võimalik panna panna teisi inimesi vaiksemaks või valjemaks. Selle jaoks on vaja teha parem hiireklikk selle inimese nime peal, kus leidub <i>User Volume</i> valik ning selle all oleva valget täppi lohistades vasakule läheb see isik vaiksemaks ning paremale lohistades valjemaks. Niimoodi saab muuta teise inimese heli ainult teie jaoks, teisi inimesi see ei mõjuta.</p>        
+                    </TextGroup>
+                </Flex>
+                <Flex>
+                    <TextGroup>
+                    <Title>Enda mikrofoni ja heli vaigistamine</Title>
+                        <p>Enda mikrofoni on võimalik discordis vaigistada, selle valiku leiab ekraani alumises osas vahetult teie Discordi nime paremal, kus on mikrofoni ikoon ja kõrvaklappide ikoon. Kui mikrofoni peal on diagonaalne punane joon, tähendab see, et teie olete juba vaigistatud. Selle ikooni peale vajutades saab mikrofoni sisse ja välja lülitada. Kõrvaklappide ikooni peale vajutades saab Discordi heli välja lülitada.</p>        
+                    </TextGroup>
+                </Flex>
+                <Flex>
+                    <TextGroup>
                     <Title>Firmaga rääkimine</Title>
                         <p>Kui soovite rääkida firmade esindajatega, liituge vastava firma helikanaliga, mille nimes on <i>queue</i>. Firmade esindajad ise valivad mitu inimest saab nende helikanalis korraga viibida ning kui seal on vabu kohti, siis saab sellega liituda. Helikanaliga liitudes on eesõigus nendel, kes on kauem oodanud. Samuti ei soovita ühe firmaga liiga kaua rääkida, et ka teised huvilised saaksid võimaluse.</p>
                     </TextGroup>    
