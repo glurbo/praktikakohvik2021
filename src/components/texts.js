33e6e7ba9d9764533d586fc23a740e1cb8ef5131
germo <glinde@ttu.ee>
2021-03-05 00:01:04 +0200

    texts

---
+++ src/components/texts.js
@@ -0,0 +1,110 @@
+const text = {
+    en: {
+        navigation: {
+            title: "INTERNSHIP CAFÉ 2021",
+            registration: "Registration",
+            guide: "Discord guide"
+        },
+        header: {
+            title: "FIND YOURSELF AN INTERNSHIP",
+            secondaryTitle: "17.03 • 12:00 p.m. • Discord",
+            content: "Internship café is an information day for IT majors. Students have the moment to learn more about different companies and career opportunities as well as find an internship of their dreams. Join the event’s Discord and be a part of TalTech's largest internship information day."
+        },
+        about: {
+            title: "About the event",
+            content: "<p>Internship café is an information day for TalTech students, where representatives of different companies and future interns meet. Those interested have the opportunity to ask specialists about their work and get to know their potential employers.</p><p>PS! Internship café is primarily aimed at IT students, but students from other faculties can also find something of interest. The event is organized by TalTech School of IT, TalTech IT College and The Student Council of the School of IT.</p>"
+        },
+        companies: {
+            title: "Participating companies"
+        },
+        questions: {
+            title: "What to ask?",
+            subtext: "Here are some examples of questions to ask employers.",
+            content: "<li>What skills are required to apply for a position?</li><li>What technologies are used in the company?</li><li>What to expect in terms of salary?</li><li>What does the daily work of the office look like?</li><li>What is the workload?</li><li>How flexible is the schedule or workload when working as a student?</li><li>What are the self-development opportunities in the company? Are there any additional trainings offered?</li>"
+        },
+        form: {
+            name: "Name",
+            namePlaceholder: "Enter name",
+            email: "Email",
+            emailPlaceholder: "Enter email",
+            button: "Submit"
+        },
+        guide: {
+            textBox1: {
+                title: "Voice channels and text channels",
+                content: "There are 2 ways of communication in Discord, voice channels and text channels. These can be found on the left side of the screen, just right of the discord server icons. Text channels begin with the “<b>#</b>” symbol, but pay attention that texts end up in the correct text channel! Voice channels appear with a speaker icon, if you click on it, you will automatically join the voice channel right away and ready to speak. If for some reason other people can’t hear you or sound quality isn’t good enough, contact the moderators and we will try to find the solution for you!"
+            },
+            textBox2: {
+                title: "Someone too loud or too quiet?",
+                content: "Discord allows you to change other peoples volume if it is too quiet or too loud. (Note other people will not get affected by this.) To achieve this, you need to hover over the persons name and right-click, then you will see the <i>User Volume</i> option appear in the pop-up menu, dragging the white dot to the left will lower the users volume and dragging the dot to the right will raise the volume."
+            },
+            textBox3: {
+                title: "Muting yourself and deafening Discord",
+                content: "To mute yourself in Discord, locate the microphone icon on the bottom of the screen directly to the right of your name. If there is a diagonal red line across the microphone icon, that means that you’re already muted. Clicking on the icon will toggle your microphone on and off. The headphones icon will deafen Discord and you will not hear anyone nor will anyone hear you."
+            },
+            textBox4: {
+                title: "Speaking to a company",
+                content: "If you wish to speak with a representative from a company, join the voice channel with the corresponding voice channel that also has “queue” in its name. The channel with the representatives in it has a maximum capacity of 5 people and if there is less than 5 people in the channel, then you can join said channel. Please let the people who have been in the queue channel longer join first. The representatives might notice you skipping line and that will leave a bad impression on them. If you’ve talked for a while with the same company, please leave the channel so that other interested people can join."
+            },
+            textBox5: {
+                title: "Questions and problems",
+                content: "If you have any questions or problems, then type in the #help text channel and moderators will try to find a solution as fast as possible."
+            },
+        }
+    },
+    et: {
+        navigation: {
+            title: "PRAKTIKAKOHVIK 2021",
+            registration: "Registreerimine",
+            guide: "Discordi juhend"
+        },
+        header: {
+            title: "LEIA ENDALE PRAKTIKAKOHT",
+            secondaryTitle: "17.03 • kell 12:00 • Discordis",
+            content: "Praktikakohvik on IT-tudengitele suunatud infopäev. Tudengitel on võimalus lähemalt tutvuda erinevate ettevõtete ja karjäärivõimalustega ning leida omale unistuste praktikakoht. Liitu ürituse Discordiga ja saa osa TalTechi suurimast praktika infopäevast."
+        },
+        about: {
+            title: "Üritusest",
+            content: "<p>Praktikakohvik on TalTechi tudengitele suunatud infopäev, kus kohtuvad ettevõtete esindajad ja tulevased praktikandid. Huvilistel on võimalus küsida spetsialistidelt nende tööd puudutavaid küsimusi ning lähemalt tutvuda oma potentsiaalsete tööandjatega. Üritus toimub Discordis, registreerijatele saadetakse kutse.</p><p>PS! Praktikakohvik on eelkõige suunatud IT-tudengitele, kuid ka teiste teaduskondade üliõpilased võivad leida enda jaoks midagi huvitvat. Üritust korraldab IT-teaduskonna üliõpilaskogu.</p>"
+        },
+        companies: {
+            title: "Osalevad firmad"
+        },
+        questions: {
+            title: "Mida küsida?",
+            subtext: "Siin on mõned näited küsimustest, mida esitada tööandjatele.",
+            content: "<li>Mis oskusi on vaja, et mingile kohale kandideerida?</li><li>Milliseid tehnoloogiad firmas kasutatakse?</li><li>Mida oodata palga suhtes?</li><li>Milline näeb välja kontori igapäev?</li><li>Milline on töökoormus?</li><li>Kui paindlik on tudengina tööl käimise juures graafik või koormus?</li><li>Mis on ettevõttes arenemisvõimalused? Kas pakutakse mingeid lisakoolitusi?</li>"
+        },
+        form: {
+            name: "Nimi",
+            namePlaceholder: "Sisesta nimi",
+            email: "Email",
+            emailPlaceholder: "Sisesta email",
+            button: "Esita"
+        },
+        guide: {
+            textBox1: {
+                title: "Helikanalid ja tekstikanalid",
+                content: "Discordis on võimalik rääkida kahel viisil, helikanalites ja tekstikanalites. Need leiduvad ekraani vasakul serverite ikoonidest paremal. Tekstikanalid algavad <b>#</b>-ga. Tuleb tähele panna, et õige tekst jõuaks õigesse kanalisse! Helikanalite ees on kõlari ikoon, kui sellele vajutate, olete koheselt helikanalis ning valmis rääkima. Kui mingil põhjusel ei ole Teid kuulda või helikvaliteet ei ole see, mis ta võiks olla, siis andke moderaatoritele teada, proovime leida sellele probleemile lahenduse!"
+            },
+            textBox2: {
+                title: "Keegi liiga vali või vaikne?",
+                content: "Discordis on võimalik panna teisi inimesi vaiksemaks või valjemaks. Selle jaoks on vaja teha parem hiireklikk inimese nime peal, kus leidub <i>User Volume</i> valik ning selle all olevat valget täppi vasakule lohistades läheb selle isiku mikrofon vaiksemaks ning paremale lohistades valjemaks. Niimoodi saab muuta teise inimese heli ainult Teie jaoks, teisi inimesi see ei mõjuta."
+            },
+            textBox3: {
+                title: "Enda mikrofoni ja heli vaigistamine",
+                content: "Enda mikrofoni vaigistamise valiku leiab ekraani alumisest osast vahetult Teie Discordi nime paremal pool, kus on mikrofoni ja kõrvaklappide ikoon. Kui mikrofoni peal on diagonaalne punane joon, olete juba vaigistatud. Selle ikooni peale vajutades saab mikrofoni sisse ja välja lülitada. Kõrvaklappide ikooni peale vajutades saab Discordi heli välja lülitada."
+            },
+            textBox4: {
+                title: "Firmaga rääkimine",
+                content: "Kui soovite rääkida firmade esindajatega, liituge vastava firma helikanaliga, mille nimes on <i>queue</i>. Ühes firma helikanalis saab korraga olla kuni 5 inimest. Kui kõne liikmete arv on täis, peate ootama kuni koht vabaneb. Helikanaliga liitudes on eesõigus nendel, kes on kauem oodanud. Samuti ei soovita ühe firmaga liiga kaua rääkida, et ka teised huvilised saaksid võimaluse."
+            },
+            textBox5: {
+                title: "Küsimused ja probleemid",
+                content: "Kui tekib probleeme või küsimusi, siis kirjutage #help tekstikanalisse ning Praktikakohviku moderaatorid on kohe valmis aitama."
+            },
+        }
+    }
+}
+
+export default text;
